c699f9575f89cac121142313720f6ac30e1d436f
Gabriel Barbosa <gabrieldecarvalhu@gmail.com>
2025-03-26 11:33:49 -0300

    add: imagens 192x192 e 512x512

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3hkoq4mjql"
+    "revision": "0.qf1onnps7hg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
